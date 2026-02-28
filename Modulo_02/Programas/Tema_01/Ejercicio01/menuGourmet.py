65e21ba95c01d3bce6b04da69b4ea585c47b8169
Pablo Pérez <pablo.parezparedes@outlook.com>
2026-02-28 12:30:44 -0600

    Agregando ejercicios y temas vistos

---
+++ Modulo_02/Programas/Tema_01/Ejercicio01/menuGourmet.py
@@ -0,0 +1,22 @@
+nombreEntrante = "Totopos de Autor en Reducción de Chile y Tomate"
+precioEntrante = '150'
+
+nombrePrincipal = "Texturas de res y maíz en armonía guerrerense"
+precioPrincipal = '200'
+
+nombrePostre = "Sorbet de fresa heirloom con notas cítricas"
+precioPostre = '80'
+
+print("Menú del Día - Restaurante Gourmet")
+print("-- Entrante --")
+print(nombreEntrante)
+print("Precio:")
+print(precioEntrante + " pesos")
+print("-- Plato Principal --")
+print(nombrePrincipal)
+print("Precio:")
+print(precioPrincipal + " pesos")
+print("-- Postre --")
+print(nombrePostre)
+print("Precio:")
+print(precioPostre + " pesos")
---
+++ Modulo_02/Programas/Tema_01/Ejercicio02/cartaDeAmor.py
@@ -0,0 +1,14 @@
+parrafo1 = "Cada día te quiero más y nunca lo he olvidado."  
+parrafo2 = "En el corazón de una historia de amor tecnológica, Lucas y Carla compartían su pasión por la programación. Cada día, mientras aprendían a programar, sus corazones se sincronizaban con cada línea de código."   
+parrafo3 = "En cada amanecer, veo el brillo de tus ojos; en cada atardecer, siento el calor de tu abrazo; y en cada noche estrellada, me pierdo en el infinito de tu amor." 
+
+primerParrafo = parrafo1[1::18]
+
+segundoParrafo = parrafo2[138:148]
+
+tercerParrafo = parrafo3[125] + parrafo3[94] + parrafo3[35] + parrafo3[107] + parrafo3[20] + parrafo3[1]
+
+print("Mensajes codificados")
+print("Mensajes Secreto 1: \n", primerParrafo)
+print("Mensajes Secreto 2: \n", segundoParrafo)
+print("Mensajes Secreto 3: \n", tercerParrafo)
