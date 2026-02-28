ed2997f13406adb35f85b3f006bf03c5852f7374
Pablo Pérez <pablo.parezparedes@outlook.com>
2026-02-28 10:06:09 -0600

    first commit

---
+++ Modulo 02/Programas/Tema_0/hello_world_test_func.py
@@ -0,0 +1,6 @@
+import test_file
+from test_file import test_fun as tf
+
+tf('Hello, World as tf!')
+
+test_file.test_fun('Hello, World!')
---
+++ Modulo 02/Programas/Tema_0/test_file.py
@@ -0,0 +1,5 @@
+def test_fun(arg0):
+    print(f'arg0: {arg0}')
+
+def __main__():
+    test_fun('Hello, World!')
