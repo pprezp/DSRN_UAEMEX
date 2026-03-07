d15685c6b2fe6ed401b6b73181a0e963e15071a0
Pablo Perez <pablo.prezparedes@outlook.com>
2026-03-06 19:58:34 -0600

    Subiendo ejercicios tema comentarios y avance de funciones

---
+++ Modulo_02/Programas/Tema_03/Comentarios.py
@@ -0,0 +1,18 @@
+textoCapitanGarcia = 'Este lugar es más impresionante de lo que imaginé.'
+tecxtoTripulacion = 'Debemos ser cuidadosos.'
+textoIngRodrigue = 'No sabemos qué tipo de microorganismos podrían existir aquí.'
+
+"""
+Primera parte:
+La Expedición a Marte ha sido un éxito. 
+Los astronautas han aterrizado y se preparan para explorar el terreno.
+"""
+
+# Segunda parte:
+#Capitán García observa el paisaje marciano y piensa: 
+
+print(f"Capitan garcía: '{textoCapitanGarcia}'")
+print(f"Tripulación: '{tecxtoTripulacion}'")
+
+#La ingeniera Rodriguez advierte:
+print(f"ngeniera Rodriguez: '{textoIngRodrigue}'")
